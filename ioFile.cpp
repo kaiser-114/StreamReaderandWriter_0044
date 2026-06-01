d5cdc7b4b79ea69eba4822db023da87cbc9f6e95
kaiser-114 <areezilla@gmail.com>
2026-06-01 10:14:34 +0700

    Implement file writing and reading in main

--- ioFile.cpp
+++ ioFile.cpp
@@ -6,5 +6,23 @@ using namespace std;
 int main() {
     string baris;
     // membuka file dalam mode menulis.
-   
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    //unlimited loop untuk menulis
+    while (true) {}
+    //selesai dalam menulis sekarang tutup filenya
+        outfile.close();
+        //Membuka file dalam mode membaca
+        ifstream infile;
+        //menunjuk ke sebuah file
+        infile.open("contohfile.txt");
+        cout << endl << ">= Pembuka  dan membaca file " << endl;
+        //jika file ada maka
+        if( infile.is_open())
+    {}
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
