afd7c552d5fd329c5507abc8901c0dce3bca0a92
kaiser-114 <areezilla@gmail.com>
2026-06-01 10:21:46 +0700

    Implement file writing and reading

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -9,5 +9,40 @@ int main() {
     cout << "Masukkan Nama File : ";
     cin >> NamaFile;
     //membuka file dalam mode menulis.
-   
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + "txt", ios::out);
+    cout << ">= Menulis file \'q' untuk keluar" << endl;
+    //unlimited loop untuk menulis
+    while (true) {
+    cout << "- ";
+    //mendapatkan setiap karakter dalam satu baris
+    getline(cin, baris);
+    //loop akan berhenti jika anda masukkan karakter q
+    if (baris == "q") break;
+    //menulis dan memasukkan nilai dari 'baris' ke dalam ke dalam file 
+    outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+    //Membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open(NamaFile, ios::in);
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+     if(infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tuutp file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
