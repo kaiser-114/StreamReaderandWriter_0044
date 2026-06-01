7f06afb41892eb8066b398613ca85070391ea24f
kaiser-114 <areezilla@gmail.com>
2026-06-01 10:15:49 +0700

    Fix file reading logic to display

--- ioFile.cpp
+++ ioFile.cpp
@@ -29,7 +29,17 @@ int main() {
         cout << endl << ">= Pembuka  dan membaca file " << endl;
         //jika file ada maka
         if( infile.is_open())
-    {}
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        } 
+        //tutup file tersebut setelah selesai
+        infile.close();       
+    }
     //jika tidak ditemukan file maka akan menampilkan ini
     else cout << "Unable to open file";
     return 0;
